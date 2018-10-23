45e72d1e63ef252774fb886c432e2ee650a32d2b
youdi <liangchangyoujackson@gmail.com>
2018-10-23 09:22:54 +0800

    truffle

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -1,7 +1,25 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.24;
+
+//import "../../v1/this.sol";
 
 contract Adoption {
-    function Adoption(){
 
+    address[16] public adopters; //保存领养者的地址
+
+    // 领养宠物
+    function adpot(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
     }
+
+    // 返回领养的宠物
+    function getAdopters() public view returns (address[16]) {
+        return adopters;
+    }
+
 }
+
+
